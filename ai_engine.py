21d960e642b046592cca13bb1a5e7a06065c67d7
ashar-codes <muhammadashar0300@gmail.com>
2026-02-18 01:13:55 +0500

    Update ai_engine.py

--- ai_engine.py
+++ ai_engine.py
@@ -51,7 +51,7 @@ def analyze_resume(text):
     """
 
     response = client.chat.completions.create(
-        model="llama3-70b-8192",
+    model="llama-3.1-8b-instant",
         messages=[{"role": "user", "content": prompt}],
         temperature=0.5,
     )
